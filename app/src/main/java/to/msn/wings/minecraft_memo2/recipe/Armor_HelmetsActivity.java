ec814f615e5a806350535688910fadd032354ae5
jin3210 <jin.taguchi.sys@gmail.com>
2017-10-31 11:50:33 +0900

    test commit

--- app/src/main/java/to/msn/wings/minecraft_memo2/recipe/Armor_HelmetsActivity.java
+++ app/src/main/java/to/msn/wings/minecraft_memo2/recipe/Armor_HelmetsActivity.java
@@ -2,7 +2,13 @@ package to.msn.wings.minecraft_memo2.recipe;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 import to.msn.wings.minecraft_memo2.R;
 
@@ -12,6 +18,11 @@ import to.msn.wings.minecraft_memo2.R;
 
 public class Armor_HelmetsActivity extends Activity {
 
+    int viewId;
+    int strId;
+    int draId;
+    int imgviewId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +30,32 @@ public class Armor_HelmetsActivity extends Activity {
         Intent intent = getIntent();
         int key = intent.getIntExtra("key",0);
 
+        Resources res = getResources();
+        strId = res.getIdentifier("armor" + key,"string",getPackageName());
+        viewId = res.getIdentifier("textView","id",getPackageName());
+        draId = res.getIdentifier("armorItem" + key,"drawable",getPackageName());
+        imgviewId = res.getIdentifier("gif1","id",getPackageName());
+
         switch (key){
             case 0:
+            case 1:
+            case 2:
+            case 3:
+                setContentView(R.layout.base_wood);
+                ImageView imageView = (ImageView) findViewById(R.id.gif1);
+                GlideDrawableImageViewTarget target = new GlideDrawableImageViewTarget(imageView);
+                Glide.with(this).load(draId).into(target);
+                TextView textView = (TextView)findViewById(viewId);
+                textView.setText(res.getString(strId));
+                ImageView imageView1 = (ImageView)findViewById(imgviewId);
+                imageView1.setImageDrawable(res.getDrawable(draId));
+                break;
+            case 4:
+                setContentView(R.layout.base_wood);
+                textView = (TextView)findViewById(viewId);
+                textView.setText(res.getString(strId));
+                imageView1 = (ImageView)findViewById(imgviewId);
+                imageView1.setImageDrawable(res.getDrawable(draId));
                 setContentView(R.layout.base_wood);
                 break;
 
--- app/src/main/java/to/msn/wings/minecraft_memo2/recipe/Truck_MinecartActivity.java
+++ app/src/main/java/to/msn/wings/minecraft_memo2/recipe/Truck_MinecartActivity.java
@@ -2,7 +2,10 @@ package to.msn.wings.minecraft_memo2.recipe;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import to.msn.wings.minecraft_memo2.R;
 
@@ -12,6 +15,11 @@ import to.msn.wings.minecraft_memo2.R;
 
 public class Truck_MinecartActivity extends Activity {
 
+    int viewId;
+    int strId;
+    int draId;
+    int imgviewId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +27,28 @@ public class Truck_MinecartActivity extends Activity {
         Intent intent = getIntent();
         int key = intent.getIntExtra("key",0);
 
+        Resources res = getResources();
+        strId = res.getIdentifier("vehicle" + key,"string",getPackageName());
+        viewId = res.getIdentifier("textView","id",getPackageName());
+        draId = res.getIdentifier("vehicleItem" + key,"drawable",getPackageName());
+        imgviewId = res.getIdentifier("gif1","id",getPackageName());
+
         switch (key){
             case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                setContentView(R.layout.base_wood);
+                TextView textView = (TextView)findViewById(viewId);
+                textView.setText(res.getString(strId));
+                ImageView imageView = (ImageView)findViewById(imgviewId);
+                imageView.setImageDrawable(res.getDrawable(draId));
                 setContentView(R.layout.base_wood);
                 break;
         }
